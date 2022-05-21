2addbd0a96e159c0fe557433690657aab529badd
mateo <mateo.marchisone@gmail.com>
2022-05-21 12:00:14 -0300

    carousel dont work

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -7,12 +7,32 @@ import {Equipo} from "../../models/equipo";
   styleUrls: ['./carousel.component.sass']
 })
 export class CarouselComponent implements OnInit {
-  @Input() equipos: Equipo[] = []
+  @Input() equipos: Equipo[];
+  slides: Array<Equipo[]> = new Array<Equipo[]>();
+  slideSize: number = 3;
 
   constructor() {
+    this.equipos = [];
+  }
+
+  getSlide(slideIndex: number): Equipo[] {
+    let start = slideIndex * this.slideSize;
+    return this.equipos.slice(start, start + this.slideSize);
+  }
+
+  numberOfSlides(): number {
+    return Math.ceil(this.equipos.length / this.slideSize);
+  }
+
+  nextSlide() {
+  }
+
+  previousSlide() {
   }
 
   ngOnInit(): void {
+    for (let i = 0; i < this.numberOfSlides(); i++) {
+      this.slides.push(this.getSlide(i))
+    }
   }
-
 }
